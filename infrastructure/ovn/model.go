3ae4f6170c34ccec1917c3c47dc92bacb1122bc8
nakamuraitsuki <ituki1911n@gmail.com>
2026-08-14 10:49:12 +0900

    feat: router モデル追加

--- infrastructure/ovn/model.go
+++ infrastructure/ovn/model.go
@@ -54,15 +54,15 @@ type LogicalSwitchPort struct {
 
 	// Addressing
 	Addresses        []string `ovsdb:"addresses"`
-	DynamicAddresses *string   `ovsdb:"dynamic_addresses"`
-	PortSecurity     []string  `ovsdb:"port_security"`
+	DynamicAddresses *string  `ovsdb:"dynamic_addresses"`
+	PortSecurity     []string `ovsdb:"port_security"`
 
 	// DHCP
 	DHCPv4Options *string `ovsdb:"dhcpv4_options"`
 	DHCPv6Options *string `ovsdb:"dhcpv6_options"`
 
 	// Mirror / Health Check
-	MirrorRules  []string `ovsdb:"mirror_rules"`
+	MirrorRules []string `ovsdb:"mirror_rules"`
 
 	// HA
 	HAChassisGroup *string `ovsdb:"ha_chassis_group"`
@@ -71,9 +71,61 @@ type LogicalSwitchPort struct {
 	ExternalIDs map[string]string `ovsdb:"external_ids"`
 }
 
+type LogicalRouter struct {
+	UUID string `ovsdb:"_uuid"`
+
+	// Naming
+	Name string `ovsdb:"name"`
+
+	Ports             []string `ovsdb:"ports"`
+	StaticRoutes      []string `ovsdb:"static_routes"`
+	Policies          []string `ovsdb:"policies"`
+	Nat               []string `ovsdb:"nat"`
+	LoadBalancer      []string `ovsdb:"load_balancer"`
+	LoadBalancerGroup []string `ovsdb:"load_balancer_group"`
+
+	Copp *string `ovsdb:"copp"`
+
+	Enabled *bool `ovsdb:"enabled"`
+
+	Options map[string]string `ovsdb:"options"`
+
+	ExternalIDs map[string]string `ovsdb:"external_ids"`
+}
+
+type LogicalRouterPort struct {
+	UUID string `ovsdb:"_uuid"`
+
+	// Naming
+	Name string `ovsdb:"name"`
+
+	GatewayChassis []string `ovsdb:"gateway_chassis"`
+	HaChassisGroup *string  `ovsdb:"ha_chassis_group"`
+
+	Networks []string `ovsdb:"networks"`
+
+	MAC string `ovsdb:"mac"`
+
+	Enabled *bool `ovsdb:"enabled"`
+
+	Ipv6RaConfigs        []string `ovsdb:"ipv6_ra_configs"`
+	Ipv6Prefix           []string `ovsdb:"ipv6_prefix"`
+	Ipv6PrefixDelegation bool     `ovsdb:"ipv6_prefix_delegation"`
+
+	Peer *string `ovsdb:"peer"`
+
+	Options map[string]string `ovsdb:"options"`
+
+	Status map[string]string `ovsdb:"status"`
+
+	ExternalIDs map[string]string `ovsdb:"external_ids"`
+}
+
 func DatabaseModel() (model.ClientDBModel, error) {
 	return model.NewClientDBModel("OVN_Northbound", map[string]model.Model{
-		"Logical_Switch": &LogicalSwitch{},
+		"Logical_Switch":      &LogicalSwitch{},
 		"Logical_Switch_Port": &LogicalSwitchPort{},
+		"Logical_Router":      &LogicalRouter{},
+		"Logical_Router_Port": &LogicalRouterPort{},
 	})
 }
